426d3c0d38c1aae10bbb5accdad70f17d29845c7
William Perry <durabilliam@gmail.com>
2021-09-13 10:11:25 -0230

    add sorted hits table

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -3,7 +3,7 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
-  /** the function for which we want to count url hits **/
+  /** the function for which we want to count url hits */
   downstream: lambda.IFunction;
 }
 
@@ -12,12 +12,16 @@ export class HitCounter extends cdk.Construct {
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
+  /**hit counter table */
+  public readonly table: dynamodb.Table;
+
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
     const table = new dynamodb.Table(this, 'Hits', {
         partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
     });
+    this.table = table;
 
     this.handler = new lambda.Function(this, 'HitCounterHandler', {
         runtime: lambda.Runtime.NODEJS_14_X,
